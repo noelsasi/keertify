03500d41b8e5de2942079cd3d1d4a2f466170e4d
noelsasi <noel4ver@gmail.com>
2026-03-18 17:08:44 +0530

    FIX: web build and deployment fixes

--- apps/web/vite.config.ts
+++ apps/web/vite.config.ts
@@ -11,4 +11,11 @@ export default defineConfig({
       "@": path.resolve(__dirname, "./src"),
     },
   },
+  server: {
+    host: true,
+  },
+  preview: {
+    host: true,
+    port: process.env.PORT ? parseInt(process.env.PORT) : 4173,
+  },
 })
